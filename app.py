e8f52617aa952a281bea2f51addc061c64f5fe55
Jacques <49912455+Jacques2101@users.noreply.github.com>
2024-12-24 17:07:33 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,26 +1,32 @@
-from sqlalchemy import create_engine
 import pandas as pd
+from sqlalchemy import create_engine
+from sqlalchemy.engine.url import URL
 import streamlit as st
 
-@st.cache_data
 def lecture_bdd(DATABASE, table, colonnes=None, condition=None):
     SERVER = 'vestathena.database.windows.net'
     USERNAME = 'login_MAM_read'
     PASSWORD = 'MonceauAM2021!'
-    DRIVER = 'ODBC+Driver+17+for+SQL+Server'
-    connection_string = f'mssql+pyodbc://{USERNAME}:{PASSWORD}@{SERVER}:1433/{DATABASE}?driver={DRIVER}'
-    engine = create_engine(connection_string)
-    
+    DRIVER = 'ODBC Driver 17 for SQL Server'
+    connection_url = URL.create(
+        drivername="mssql+pyodbc",
+        username=USERNAME,
+        password=PASSWORD,
+        host=SERVER,
+        port=1433,
+        database=DATABASE,
+        query={"driver": DRIVER}
+    )
+    engine = create_engine(connection_url)
     # Construire la requête SQL
     query = f"SELECT {', '.join(colonnes) if colonnes else '*'} FROM {table}"
     if condition:
         query += f" WHERE {condition}"
-    
+
     # Lire toute la table en une seule fois
     df = pd.read_sql(query, engine)
     return df
 
-# Exemple d'utilisation
+# Utilisation
 df = lecture_bdd("vestathena_db", "benchmark_main")
-df
-
+st.dataframe(df)
