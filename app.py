f7400cc97fa417702bb1bdd405521552e0fc895b
Jacques <49912455+Jacques2101@users.noreply.github.com>
2024-12-25 11:58:50 +0100

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def run_query(query):
         cur.execute(query)
         return cur.fetchall()
 
-rows = run_query("SELECT * from mytable;")
+rows = run_query("SELECT * from benchmark_main;")
 
 # Print results.
 for row in rows:
